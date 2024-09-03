87cc7a09c5c0a7274ec8bd640c6b34ea148108c0
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-03 18:53:25 -0500

    Implement user login validation in cajero_electronico function
    Co-authored-by:  Francisco <francisco.colomer@gmail.com>
    Co-authored-by:  Felipe <fvilla.sanles@gmail.com>
    Co-authored-by:  Barbara <bhahumada@icloud.com>

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
+# Excepciones del Banco
+
+
+class NombreIncorrectoError(Exception):
+    pass
+
+
+class NumeroCuentaIncorrectoError(Exception):
+    pass
+
+
+class PinIncorrectoError(Exception):
+    pass
+
+
+# Constantes
 NOMBRE_CORRECTO = "Pepe"
 NUMERO_CUENTA_CORRECTO = "123456789"
 PIN_CORRECTO = "1234"
 
-intentos = 0
-max_intentos = 3
 
-while intentos < max_intentos:
+def cajero_electronico():
+    intentos = 0
+    max_intentos = 3
 
-    try:
-        # Ingreso de datos del usuario
-        nombre = input("Ingrese su nombre: ")
-        numero_cuenta = input("Ingrese su numero de cuenta: ")
-        pin = input("Ingrese su pin: ")
+    while intentos < max_intentos:
 
-        # Validar la información
-        if nombre != NOMBRE_CORRECTO:
-            raise ValueError("El nombre que ingresaste es incorrecto")
-        if numero_cuenta != NUMERO_CUENTA_CORRECTO:
-            raise ValueError("El numero de cuenta que ingresaste es incorrecto")
-        if pin != PIN_CORRECTO:
-            raise ValueError("El pin que ingresaste es incorrecto")
+        try:
+            # Ingreso de datos del usuario
+            nombre = input("Ingrese su nombre: ")
+            numero_cuenta = input("Ingrese su numero de cuenta: ")
+            pin = input("Ingrese su pin: ")
 
-        # Si todo va bien
-        print("Felicidades pudiste acceder a tu cuenta")
-        break
+            # Validar la información
+            if nombre != NOMBRE_CORRECTO:
+                raise NombreIncorrectoError
+            ("El nombre que ingresaste es incorrecto")
+            if numero_cuenta != NUMERO_CUENTA_CORRECTO:
+                raise NumeroCuentaIncorrectoError(
+                    "El numero de cuenta que ingresaste es incorrecto"
+                )
+            if pin != PIN_CORRECTO:
+                raise PinIncorrectoError("El pin que ingresaste es incorrecto")
 
-    except ValueError as e:
-        intentos += 1
-        print(f"Error {e}")
-        if intentos < max_intentos:
-            print(f"Tienes {max_intentos - intentos} intentos restantes")
-        else:
-            print("Alcanzaste la cantidad de intentos permitidos")
+            # Si todo va bien
+            print("Felicidades pudiste acceder a tu cuenta")
+            break
+
+        except (
+            NombreIncorrectoError,
+            NumeroCuentaIncorrectoError,
+            PinIncorrectoError,
+        ) as e:
+            intentos += 1
+            print(f"Error {e}")
+            if intentos < max_intentos:
+                print(f"Tienes {max_intentos - intentos} intentos restantes")
+            else:
+                print("Alcanzaste la cantidad de intentos permitidos")
+
+
+# invocar la función
+cajero_electronico()
